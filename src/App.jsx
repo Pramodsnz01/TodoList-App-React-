3ab2c6162904e6fc6c1f68ec7d982f0aca6fc0cc
Pramodsnz01 <pramodsnz01@gmail.com>
2025-06-13 10:24:17 +0545

    Basic structure and css

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,31 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Navbar from './components/Navbar'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-     <>
-      <div className="contatiner">
-        <div className="bg-red-600">
-          I am red
+    <>
+      <Navbar />
+      <div className='contatiner mx-8 my-5 rounded-xl p-5 bg-violet-200 min-h-[80vh]'>
+        <div className="addTodo my-5">
+          <h2 className="text-lg font-bold">Add a Todo</h2>
+          <input type="text" className='w-1/2 bg-white' />
+          <button className='bg-violet-800 hover:bg-violet-950 p-2 py-1 text-sm font-bold text-white rounded-md mx-6'>Add</button>
+        </div>
+        <h2 className='text-lg font-bold'>Your Todos</h2>
+        <div className="todos ">
+          <div className="todo flex">
+            <div className="text">Lorem ipsum dolor sit amet consectetur adipisicing.</div>
+            <div className="buttons">
+              <button className='bg-violet-800 hover:bg-violet-950 p-2 py-1 text-sm font-bold text-white rounded-md mx-1'>Edit</button>
+              <button className='bg-violet-800 hover:bg-violet-950 p-2 py-1 text-sm font-bold text-white rounded-md mx-1'>Delete</button>
+            </div>
+
+          </div>
         </div>
       </div>
-     </>
+    </>
   )
 }
 
